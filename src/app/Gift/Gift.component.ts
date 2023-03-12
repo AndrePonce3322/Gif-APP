ba1deeace3936c2b0db36ca7fef65ebbdd252613
AndrePonce3322 <andreponce417@gmail.com>
2023-03-11 18:41:56 -0600

    .

---
+++ src/app/Gift/Gift.component.ts
@@ -0,0 +1,9 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'app-gift',
+    templateUrl: './Gift.component.html',
+    styleUrls: ['./Gift.component.css']
+})
+
+export class GiftComponent { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+// Components
 import { AppComponent } from './app.component';
+import { GiftComponent } from './Gift/Gift.component';
+import {HttpClientModule } from '@angular/common/http'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GiftComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
